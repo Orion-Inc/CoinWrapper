7a1207922e796d9093c4a6f5cf5277f22fb95464
LORD-KAY <offeilord@gmail.com>
2018-09-04 15:55:46 +0000

    :white_check_mark: Writing some tests

---
+++ test/app.spec.js
@@ -0,0 +1,12 @@
+const express = require("express"),
+    request = require("superagent"),
+    expect = require("chai").expect;
+    app = express();
+
+
+    //testing the app js file
+describe('#Testing the express app itself', function () {
+    it('should display the running port', function (done) {
+
+    });
+});
--- test/utilities.spec.js
+++ test/utilities.spec.js
@@ -2,18 +2,26 @@
 let expect = require('chai').expect,
     should = require('chai').should;
 let User = require('../models/users'),
-    Resolvers = require('../utils/resolvers'),
-    User_Verification = require('../models/user_verification');
-let db = require('./utils/db.spec');
+    Resolvers = require('../utils/resolvers');
+
 
 //testing the utilities
 describe("checking the usability of the resolvers",function(){
-    it('should be a function', function () {
+    it('should be a function', function (done) {
         expect(Resolvers.generateValidToken).to.be.a('function');
+        done();
     });
 
-    it('should return a promise', function () {
+    it('should return a promise', function (done) {
         expect(Resolvers.generateValidToken(512)).to.be.a("promise");
+        done();
+    });
+
+    it('should return a string of provided bitsize', function (done) {
+        Resolvers.generateValidToken(512).then( uid => {
+            expect(uid).to.be.a("string");
+        });
+        done();
     });
 });
 	
