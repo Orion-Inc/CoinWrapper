da5de05413e94b95b8d7a5a518a4ca8984176b04
LORD KAY <offeilord@gmail.com>
2018-09-09 13:59:06 +0000

    :sparkles: New Feature , add controllers to structure the files more

--- api/swap-v1.js
+++ api/swap-v1.js
@@ -3,14 +3,15 @@ require('module-alias/register');
 let express = require("express"),
     SignUpRouter = require("./v1/auth/signup"),
     SignInRouter = require("./v1/auth/signin"),
+    AuthorizationGuard = require("@guards/authorizationGuard"),
     app = express();
 
 let apiAuth = express.Router();
 //Sign up and login route will be here
 apiAuth.use('/auth',[SignUpRouter,SignInRouter]);
 //Middleware for checking the authentication data
-
-
+apiAuth.use(AuthorizationGuard);
+//Authorization routes will appear here
 apiAuth.get('/auth/:email',function(req,res){
     let email = req.params.email;
     res.status(200).json({
