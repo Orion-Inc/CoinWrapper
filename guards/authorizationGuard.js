702ae3f6f520ef601542974b2e35c9f20e481185
LORD KAY <offeilord@gmail.com>
2018-09-09 13:44:59 +0000

    :sparkles: Configs for middleware

--- guards/authorizationGuard.js
+++ guards/authorizationGuard.js
@@ -4,6 +4,43 @@ let express = require("express"),
 
 app = express();
 authorizationGuard = express.Router();
+authorizationGuard.use(function(req,res,next){
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token,config.secret,{
+            algorithm: ['HS256']
+        },function(err,decoded){
+            if (err){
+                res.status(401)
+                    .json({
+                        message: 'Failed To Authenticate. Session has expired',
+                        results: null,
+                        success: false,
+                    });
+            } else {
+                //checking if token is expired
+                let current_time = new Date().getTime() / 1000;
+                if (current_time > decoded.expiresIn){
+                    res.status(401)
+                        .json({
+                            message: 'Session Has Expired',
+                            results: null,
+                            success: false
+                        });
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            }
+        });
+    } else {
+        res.status(401)
+            .json({
+                message: 'No Token provided',
+                success: false,
+            });
+    }
 
+});
 //Exporting the module
 module.exports = authorizationGuard;
