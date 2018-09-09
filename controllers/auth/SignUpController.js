57a0d4464924e5c399f1b5a7a97bbd4225f7752b
LORD KAY <offeilord@gmail.com>
2018-09-09 14:54:28 +0000

    :tada: Separation of concerns

--- controllers/auth/SignUpController.js
+++ controllers/auth/SignUpController.js
@@ -8,51 +8,52 @@ let express = require("express"),
     config = require("@conn/connectionString");
     app = express();
 
-let SignUpController = {
-    index: function(req,res){
+let SignUpController;
+SignUpController = {
+    index: function (req, res) {
         let Query = new Users({
             firstname: req.body.firstname,
-            othername :  req.body.othername,
-            username : req.body.username,
-            email  : req.body.email,
-            phone :  req.body.phone
+            othername: req.body.othername,
+            username: req.body.username,
+            email: req.body.email,
+            phone: req.body.phone
         });
 
         //Check if the user already exists
-        Users.findOne({ email: Query.email , phone: Query.phone },function(err, user){
-            if (err){
+        Users.findOne({email: Query.email, phone: Query.phone}, function (err, user) {
+            if (err) {
                 throw err;
             }
 
-            if (!user){
-                Query.save(function(err,results){
-                    if (!err){
+            if (!user) {
+                Query.save(function (err, results) {
+                    if (!err) {
                         // Using jwt instead for the token generation
                         const payload = {
                             user_id: results._id
                         };
-                        let uriToken = jwt.sign(payload,config.secret,{
+                        let uriToken = jwt.sign(payload, config.secret, {
                             algorithm: 'HS256',
                             expiresIn: '300s'
                         });
                         // A four digit verification token
-                        let genToken = Resolvers.verificationCode(10000,99999);
+                        let genToken = Resolvers.verificationCode(10000, 99999);
 
                         let Verification = new User_Verification({
                             token: genToken,
                             user_id: results._id
                         });
 
-                        Verification.save(function(err,data){
-                            if (!err){
+                        Verification.save(function (err, data) {
+                            if (!err) {
                                 const fullname = results.firstname + "  " + results.othername;
-                                tokenNotifier('lordkay1996@gmail.com',results.email,fullname,uriToken,"Signing Up");
+                                tokenNotifier('lordkay1996@gmail.com', results.email, fullname, uriToken, "Signing Up");
 
                                 res.status(201).json({
-                                    message: "User Profile Successfully Created\n An Email has been sent to your "+ results.email + " .",
+                                    message: "User Profile Successfully Created\n An Email has been sent to your " + results.email + " .",
                                     success: true
                                 });
-                            }else{
+                            } else {
                                 res.status(401)
                                     .json({
                                         message: 'An error occurred while saving access token',
@@ -60,7 +61,7 @@ let SignUpController = {
                                     });
                             }
                         });
-                    }else {
+                    } else {
                         console.log(err);
                         res.status(401)
                             .json({
@@ -69,7 +70,7 @@ let SignUpController = {
                             });
                     }
                 })
-            } else if(user){
+            } else if (user) {
                 res.status(401).json({
                     message: 'User already Exists',
                     success: false
