947d97e141faf29285ef7fe336eebf22acb44de3
LORD KAY <offeilord@gmail.com>
2018-10-14 04:31:49 +0000

    Add the route mouthpiece of the create ad endpoint to the dashboard.js file in the api/ folder

--- api/v1/user/dashboard.js
+++ api/v1/user/dashboard.js
@@ -1,10 +1,14 @@
 require("module-alias/register");
 let express = require("express"),
+    CreateAdPolicy = require("@policies/CreateAdPolicy"),
     DashboardController  = require("@controllers/user/DashboardController");
 
 dashboardRouter = express.Router();
 // Routes
 dashboardRouter.get('/authorize/:email',DashboardController.reroute);
 dashboardRouter.get('/home',DashboardController.index);
+dashboardRouter.post('/create-ad',
+    CreateAdPolicy.validate,
+    DashboardController.createAds)
 //exporting the dashboard module
 module.exports = dashboardRouter;
